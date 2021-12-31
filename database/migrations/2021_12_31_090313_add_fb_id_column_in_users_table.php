7bb6345ddd40343bedec8b321089c480a7514f2e
Amirhossein Dashtizade <amirhossein.d.z@gmail.com>
2021-12-31 17:10:22 +0330

    Added unique index for fb_id

--- database/migrations/2021_12_31_090313_add_fb_id_column_in_users_table.php
+++ database/migrations/2021_12_31_090313_add_fb_id_column_in_users_table.php
@@ -14,7 +14,7 @@ class AddFbIdColumnInUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('fb_id')->nullable();
+            $table->string('fb_id')->nullable()->unique();
         });
     }
 
