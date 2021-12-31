006f5bf150bb522ca5954a1e1c85b198293a56e2
Amirhossein Dashtizade <amirhossein.d.z@gmail.com>
2022-01-01 00:45:44 +0330

    Removed unwanted wallet methods

--- app/Http/Controllers/Dashboard/Wallet/WalletController.php
+++ app/Http/Controllers/Dashboard/Wallet/WalletController.php
@@ -5,14 +5,11 @@ namespace App\Http\Controllers\Dashboard\Wallet;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Wallet\StoreWalletRequest;
 use App\Models\Wallet\Wallet;
-use Illuminate\Http\Request;
 
 class WalletController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -21,8 +18,6 @@ class WalletController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -44,50 +39,5 @@ class WalletController extends Controller
         );
 
         return redirect(route('wallet.index'))->with('wallet_success', true);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::resource('wallet', WalletController::class);
+    Route::resource('wallet', WalletController::class)->only(['index', 'create', 'store']);
 });
 
 require __DIR__.'/auth.php';
